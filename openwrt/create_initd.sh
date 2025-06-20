0a46a2a26b764eab52388b4a9f79301a29f9850b
fiko <fiko@example.com>
2025-06-20 16:03:15 +0800

    fix: 修复 init.d 脚本

--- openwrt/create_initd.sh
+++ openwrt/create_initd.sh
@@ -11,14 +11,7 @@ STOP=10
 start_service() {
     procd_open_instance
     
-    # 加载环境变量文件
-    if [ -f /etc/sing-box/run_env ]; then
-        while IFS='=' read -r key value; do
-            [ -n "$key" ] && [ -n "$value" ] && procd_set_param env "$key=$value"
-        done < /etc/sing-box/run_env
-    fi
-    
-    procd_set_param command /usr/bin/sing-box run -c /etc/sing-box/config.json
+    procd_set_param command /root/singbox/just_run.sh
     procd_set_param respawn
     procd_close_instance
 }
